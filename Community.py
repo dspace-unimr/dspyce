aa1fafcd3cb8a1d2b0f2db5e5868499c2fe1034e
Eike Martin Löhden <eike.loehden@ub.uni-marburg.de>
2023-12-04 14:34:01 +0100

    Add sub_community list in Community class.

--- Community.py
+++ Community.py
@@ -2,4 +2,19 @@ from . import DSpaceObject
 
 
 class Community(DSpaceObject):
-    pass
+    sub_communities: list[DSpaceObject]
+
+    def __init__(self, uuid: str = '', handle: str = '', sub_communities: list[DSpaceObject] = None):
+        super().__init__(uuid, handle)
+        self.sub_communities = sub_communities if sub_communities is not None else []
+
+    def is_subcommunity_of(self, other) -> bool:
+        """
+        Checks if the community object is a sub-community of the given community.
+        :param other: Another community object.
+        :return: True, if self is in other.sub_communities.
+        """
+        if type(other) is not Community:
+            raise TypeError('The given object must be of the type "Community".')
+        else:
+            return self in other.sub_communities
