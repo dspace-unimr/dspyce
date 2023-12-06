33d81c0a30ed374e257599460b378927c69fc6d3
Eike Martin Löhden <eike.loehden@ub.uni-marburg.de>
2023-12-06 09:42:29 +0100

    Created get_parent_community function to collections

--- Collection.py
+++ Collection.py
@@ -5,6 +5,9 @@ from . import Community
 class Collection(DSpaceObject):
     community: Community
 
-    def __init__(self, community: Community, uuid: str = '', handle: str = '', name: str = ''):
+    def __init__(self, community: Community = None, uuid: str = '', handle: str = '', name: str = ''):
         super().__init__(uuid, handle, name)
         self.community = community
+
+    def get_parent_community(self) -> Community:
+        return self.community
