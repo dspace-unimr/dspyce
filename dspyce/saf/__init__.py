3cadc44900643f260a90d8ec1308a16aa78f075d
Eike Martin Löhden <eike.loehden@ub.uni-marburg.de>
2024-02-21 15:57:17 +0100

    Move saf writing functions into saf_write.py

--- dspyce/saf/__init__.py
+++ dspyce/saf/__init__.py
@@ -3,4 +3,4 @@
 Python package for creating saf packages for DSpace item-imports and -updates.
 
 """
-from .saf import saf_packages, create_saf_package
+from .saf_write import saf_packages, create_saf_package
